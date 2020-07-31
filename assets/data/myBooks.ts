7c6a2ea127f11b54e680086cd99a7b626961bdd2
Johnny-Dieu <46733028+Johnny-Dieu@users.noreply.github.com>
2020-07-31 16:59:48 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,32 @@
+import {Books} from '../../app/bookInfo';
+
+export const BOOKS: Books[] = [
+   {
+       bname: 'The Great Gatsby',
+       bauthor: 'F. Scott Fitzgerald',
+       bgenre: 'Fiction',
+       byear: 1925,
+       bpic: 'assets/images/Books/TheGreatGatsby.jpg'
+   },
+   {
+    bname: 'Wealthing Like Rabbits',
+    bauthor: 'Robert R. Brown',
+    bgenre: 'Humor',
+    byear: 2014,
+    bpic: 'assets/images/Books/WealthingLikeRabbits.jpg'
+},
+{
+    bname: 'How To American',
+    bauthor: 'Jimmy O. Yang',
+    bgenre: 'Autobiography',
+    byear: 2018,
+    bpic: 'assets/images/Books/HowToAmerican.jpg'
+},
+{
+    bname: 'Side Hustle',
+    bauthor: 'Chris Guillebeau',
+    bgenre: 'Self Help',
+    byear: 2017,
+    bpic: 'assets/images/Books/SideHustle.jpg'
+}
+];
